ee09c10c13c1de8639edf046b1978e33848b1cd2
Bernardo Nassr Conte <138223072+bernardonc07@users.noreply.github.com>
2025-06-18 09:01:32 -0300

    Add files via upload

---
+++ OOP - Adocao/Sistema de adoção/Classes/animal.classe.php
@@ -0,0 +1,44 @@
+<?php
+class animal{
+    public $nome;
+    public $especie;
+    public $idade;
+    public $sexo;
+    public $cor;
+    public $status;
+    public $chipado;
+
+    public function exibirFicha():string{
+        return
+    }
+
+    public function atualizarPeso($peso):float{
+        return
+    }
+
+    public function alterarStatus($novoStatus):string{
+        return
+    }
+
+    public function verificarIdade():int{
+        return
+    }
+
+    public function marcarComoChipado():string{
+        return
+    }
+
+    public function desmarcarChipado():string{
+        return
+    }
+
+    public function resumoAnimal():string{
+        return
+    }
+
+    public function retornarSexo():string{
+        return
+    }
+
+
+}
---
+++ OOP - Adocao/Sistema de adoção/oop.php
@@ -0,0 +1,46 @@
+<?php
+include_once './Classes/adotante.classe.php';
+include_once './Classes/animal.classe.php';
+include_once './Classes/cachorro.classe.php';
+include_once '.Classes/gato.classe.php';
+
+$adotante = new adotante();
+$adotante->nome = "Bernardo Nassr Conte"
+$adotante->idade = 17
+$adotante->telefone = 51991839410
+$adotante->email = "bernardonassrconte@gmail.com"
+$adotante->endereco = "Av Porto Alegre Nº666, Porto Alegre "
+$adotante->temOutrosAnimais = "Sim"
+$adotante->experienciaComPets = "Sim"
+$adotante->animalAdotado = "Gato"
+
+
+$animal = new animal();
+$animal->nome = ""
+$animal->especie = 
+$animal->idade = 
+$animal->sexo = 
+$animal->peso = 
+$animal->cor = 
+$animal->status = 
+$animal->chipado = 
+
+$cachorro = new cachorro();
+$cachorro->nome = 
+$cachorro->raca = 
+$cachorro->porte = 
+$cachorro->nivelEnergia = 
+$cachorro->vacinado = 
+$cachorro->vermifugado = 
+$cachorro->sociavel = 
+$cachorro->adestrado = 
+
+$gato = new gato();
+$gato->nome = 
+$gato->pelagem = 
+$gato->castrado = 
+$gato->independente = 
+$gato->vacinado = 
+$gato->curioso = 
+$gato->adoraColo =
+$gato->usaCaixaAreia = 
